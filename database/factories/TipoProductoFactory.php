1cd9218443b4eb2ed00403e33989c521cd9427da
Diego García <diego350z1@gmail.com>
2022-05-26 10:32:30 -0600

    Creación de Modelo, Migración y Factory de tabla Tipo de Productos

---
+++ database/factories/TipoProductoFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TipoProducto>
+ */
+class TipoProductoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nombre' => $this->faker->name(),
+            'descripcion' => $this->faker->text(500),
+            'estado' => rand(0,1)
+        ];
+    }
+}
