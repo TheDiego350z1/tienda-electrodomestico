82d2b956adfba9e6b2a78db4090bb7f835d18c08
Diego García <diego350z1@gmail.com>
2022-05-26 10:43:26 -0600

    Creación de Modelo, Migración y Factory de tabla Marcas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\TipoProducto::factory(10)->create();
+        \App\Models\Marca::factory(20)->create();
 
         // \App\Models\User::factory(10)->create();
 
