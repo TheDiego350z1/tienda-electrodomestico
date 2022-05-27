06561683b1e3e185c57d2ca18a0ef6e912f8e998
Diego García <diego350z1@gmail.com>
2022-05-27 09:13:33 -0600

    Actualización de ruta / y eliminación de opción de registro

--- config/fortify.php
+++ config/fortify.php
@@ -132,7 +132,7 @@ return [
     */
 
     'features' => [
-        Features::registration(),
+        // Features::registration(), -> registro de usuarios
         Features::resetPasswords(),
         // Features::emailVerification(),
         Features::updateProfileInformation(),
